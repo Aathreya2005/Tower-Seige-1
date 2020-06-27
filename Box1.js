dce6370498d4995f8ef685a1508326b50a67871c
Aathreya2005 <66194831+Aathreya2005@users.noreply.github.com>
2020-06-27 12:03:36 +0530

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,19 @@
+class Box1 {
+  constructor(x,y) {
+    var options = {
+        isStatic:false,
+        density:0.5,
+        friction:0.8
+    }
+    this.body = Bodies.rectangle(x,y,20,30,options);
+    this.width =20
+    this.height = 30;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill("pink");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};
